e94ca77f387a111bf7661023c6dfa031d11ba94d
nguyenletan <nguyenletan@gmail.com>
2025-03-14 00:01:49 +1300

    Revert "Update dependencies and modify resume link"
    This reverts commit f7b7148fca334656a5dfed1a44d26a6a72d35aae.

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -36,7 +36,7 @@ export const DATA = {
   navbar: [
     { href: "/", icon: HomeIcon, label: "Home" },
     // { href: "/blog", icon: NotebookIcon, label: "Blog" },
-    { href: "/tannguyenlepdf", icon: FileText, label: "Resume" },
+    { href: "/tannguyenle.pdf", icon: FileText, label: "Resume" },
   ],
   contact: {
     email: "nguyenletan@gmail.com",
