1df3a140b6523afd8356d8d4ee8fd59c920152cc
nguyenletan <nguyenletan@gmail.com>
2025-07-07 08:32:01 +1200

    adjust resume card layout styles and add spacing in globals.css

--- src/components/resume-card.tsx
+++ src/components/resume-card.tsx
@@ -60,9 +60,9 @@ export const ResumeCard = ({
             <AvatarFallback>{altText[0]}</AvatarFallback>
           </Avatar>
         </div>
-        <div className="flex-grow ml-4 items-center flex-col group">
+        <div className="ml-4 flex flex-col group flex-grow">
           <CardHeader>
-            <div className="flex items-center justify-between gap-x-2 text-base">
+            <div className="flex items-center justify-items-start gap-x-2 text-base">
               <h3 className="inline-flex items-center justify-center font-semibold leading-none text-xs sm:text-sm">
                 {title}, {location}
                 {badges && (
